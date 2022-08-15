77cf69a756bcf97aa36346f9ee865f59c802db42
nanijiayou <heyuanjiayou@gmail.com>
2022-08-16 01:31:55 +0800

    feat(.): add goroutine part
    add goroutine part

--- goroutine/goroutine.go
+++ goroutine/goroutine.go
@@ -1,7 +1,38 @@
 package goroutine
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
+
+func newWork() {
+	i := 0
+	for {
+		i++
+		fmt.Printf("new goroutine : i =%d\n", i)
+		time.Sleep(1 * time.Second)
+	}
+}
 
 func TestGoroutine() {
 	fmt.Println("============= goroutine ===============")
+	go func() {
+		defer fmt.Println("A.defer")
+		func() {
+			defer fmt.Println("B.defer")
+			runtime.Goexit()
+			fmt.Println("B called")
+		}()
+		fmt.Println("A called")
+	}()
+
+	go newWork()
+
+	i := 0
+	for {
+		i++
+		fmt.Printf("main goroutine i = %d\n", i)
+		time.Sleep(1 * time.Second)
+	}
 }
